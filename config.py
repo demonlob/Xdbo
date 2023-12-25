1b2a62cc80e8f7fed4a8a530cae71ec0c540c376
demonlob <142598456+demonlob@users.noreply.github.com>
2023-12-25 18:27:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @Sophia_x_MusicBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6938549393))
+OWNER_ID = int(getenv("OWNER_ID", 6780354996))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AbhiModszYT/SophiaMusic",
+    "https://github.com/demonlob/Xdbo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
